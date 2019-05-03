6bf007b71a0e4a1cb8287f39fe0b903d28883555
Igor Pechenkin <ipechenkin@gmail.com>
2019-05-03 22:41:04 +0300

    fix. Добавлен знак !

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 // Спрашиваем у пользователя имя, далее приветствуем его
 const greeting = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}`);
+  console.log(`Hello, ${userName}!`);
 };
 
 export default greeting;
