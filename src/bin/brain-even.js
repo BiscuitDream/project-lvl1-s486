91d58da17edc0bcc1ae6622e0ae2df775c8659a4
Igor Pechenkin <ipechenkin@gmail.com>
2019-05-03 22:43:02 +0300

    Добавлена игра 'Проверка на четность'

---
+++ src/bin/brain-even.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if number even otherwise answer "no".');
+console.log();
+
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
+console.log();
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max + 1 - min)) + min;
+
+const evenGame = (count) => {
+  if (count >= 3) {
+    return console.log(`Congratulations, ${name}!`);
+  }
+
+  const secret = getRandomInt(0, 100);
+  const isEven = secret % 2 === 0;
+
+  console.log(`Question: ${secret}`);
+
+  const answer = readlineSync.question('Your answer: ');
+  if ((answer === 'yes' && isEven) || (answer === 'no' && !isEven)) {
+    console.log('Correct!');
+  } else {
+    const correct = isEven ? 'yes' : 'no';
+    const message = `'${answer}' is wrong answer ;(. Correct answer was '${correct}'. \nLet's try again, ${name}`;
+    return console.log(message);
+  }
+
+  return evenGame(count + 1);
+};
+
+evenGame(0);
