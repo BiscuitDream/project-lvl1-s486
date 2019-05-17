d7dada807d732a12fd13faeed03747afa83b1f9f
Igor Pechenkin <ipechenkin@gmail.com>
2019-05-17 23:51:54 +0300

    Правки в модуле progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,41 +2,33 @@ import { cons } from 'hexlet-pairs';
 import getRandomInt from '../utils';
 import core from '..';
 
+const length = 10;
+
 const progressionDescription = 'What number is missing in the progression?';
 
 const progressionQuestion = () => {
-  const progressionLength = 10; // можно на уровень модуля
-  const progressionStart = getRandomInt(1, 100);
-  const progressionStep = getRandomInt(1, 5);
-  const progressionSecretIndex = getRandomInt(0, 9); // hidden element index  (9 = length - 1)
+  const start = getRandomInt(1, 100);
+  const step = getRandomInt(1, 5);
+  const hiddenElementIndex = getRandomInt(0, length - 1);
 
-  const getPhrase = (element, count, acc) => {
-    if (count >= progressionLength) {
+  const getQuestion = (element, count, acc) => {
+    if (count >= length) {
       return acc;
     }
-    const newElement = (count === progressionSecretIndex) ? '..' : element + progressionStep;
-    const newAcc = `${acc} ${newElement}`; // лишний пробел будет в начале всегда
+    const newElement = (count === hiddenElementIndex) ? '..' : element + step;
+    const newAcc = `${acc} ${newElement}`;
 
-    return getPhrase(element + progressionStep, count + 1, newAcc);
+    return getQuestion(element + step, count + 1, newAcc);
   };
 
-  const getCorrectAnswer = (current, count) => {
-    if (count > progressionSecretIndex) {
-      return current;
-    }
-    const newCurrent = current + progressionStep;
+  const getElementByIndex = () => start + (step * hiddenElementIndex);
 
-    return getCorrectAnswer(newCurrent, count + 1); // посчитать через формулу, а не перебирать
-  };
+  const question = getQuestion(start - step, 0, '').trim();
+  const correctAnswer = String(getElementByIndex());
 
-  const phrase = getPhrase(progressionStart, 0, '');
-  const correctAnswer = String(getCorrectAnswer(progressionStart, 0));
-
-  return cons(phrase, correctAnswer);
+  return cons(question, correctAnswer);
 };
 
 const runProgression = () => core(progressionDescription, progressionQuestion);
 
 export default runProgression;
-
-// нет смысла добавлять прогрессион к каждой переменной. модуль и так относится в прогрессион
