f99b11289db51435f3e78f3eed7de91a02f9e9d7
Igor Pechenkin <ipechenkin@gmail.com>
2019-05-23 08:55:58 +0300

    Отрефакторил функцию getQuestion, сделал ее детерминированной

--- src/games/progression.js
+++ src/games/progression.js
@@ -6,22 +6,37 @@ const length = 10;
 
 const progressionDescription = 'What number is missing in the progression?';
 
+const getProgression = (firstElement, progressionStep, progressionLength, indOfHidden) => {
+  const iter = (element, count, acc) => {
+    if (count >= progressionLength) {
+      return acc.trim();
+    }
+
+    const newElement = (count === indOfHidden) ? '..' : element;
+    const newAcc = `${acc} ${newElement}`;
+
+    return iter(element + progressionStep, count + 1, newAcc);
+  };
+
+  return iter(firstElement, 0, '');
+};
+
 const progressionQuestion = () => {
   const start = getRandomInt(1, 100);
   const step = getRandomInt(1, 5);
   const hiddenElementIndex = getRandomInt(0, length - 1);
 
-  const getQuestion = (element, count, acc) => {
-    if (count >= length) {
-      return acc.trim();
-    }
-    const newElement = (count === hiddenElementIndex) ? '..' : element;
-    const newAcc = `${acc} ${newElement}`;
+  // const getQuestion = (element, count, acc) => {
+  //   if (count >= length) {
+  //     return acc.trim();
+  //   }
+  //   const newElement = (count === hiddenElementIndex) ? '..' : element;
+  //   const newAcc = `${acc} ${newElement}`;
 
-    return getQuestion(element + step, count + 1, newAcc);
-  };
+  //   return getQuestion(element + step, count + 1, newAcc);
+  // };
 
-  const question = getQuestion(start, 0, '');
+  const question = getProgression(start, step, length, hiddenElementIndex);
   const correctAnswer = String(start + (step * (hiddenElementIndex)));
 
   return cons(question, correctAnswer);
